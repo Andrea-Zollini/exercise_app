5a2b0f1f0bf08d9a88b550dd84828e088a2d0a73
andrea <andrea.zollini@proton.me>
2024-05-20 23:28:56 +0200

    logout functionality

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,7 @@
 <x-layouts.app title="Dashboard">
     <h1 class="font-3xl">Hi {{Auth::user()->name}}</h1>
+    <form action="{{route('logout')}}" method="POST">
+        @csrf
+        <button class="bg-orange-600 hover:bg-orange-800 text-gray-200 font-bold py-2 px-4 rounded shadow-md" type="submit">log out</button>
+    </form>
 </x-layouts.app>
